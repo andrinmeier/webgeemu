0c8b51f2e468cdfcd06f950efc6d32c295080332
Andrin Meier <andrin@pricemeier.ch>
2022-08-06 05:16:02 +0200

    Remove prisma

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,6 @@ import {
 } from "@remix-run/react";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -21,12 +20,6 @@ export const meta: MetaFunction = () => ({
   title: "Remix Notes",
   viewport: "width=device-width,initial-scale=1",
 });
-
-export async function loader({ request }: LoaderArgs) {
-  return json({
-    user: await getUser(request),
-  });
-}
 
 export default function App() {
   return (
