1cfccf00f236014c0032191725854091665545a6
Andrin Meier <andrin@pricemeier.ch>
2022-08-07 09:39:23 +0200

    Fix unit tests

---
+++ test/app/example.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+
+describe('reading messages', () => {
+
+    it('should get the latest message with a spy', () => {
+        expect(true).toEqual(true);
+    })
+})
